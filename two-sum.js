2e79eaf02550ebe4f10f2a514357432c9aee8efa
wbs7644 <61130488+wbs7644@users.noreply.github.com>
2022-06-21 11:06:55 +0900

    chap1. two-sum width javascript

--- two-sum.js
+++ two-sum.js
@@ -3,10 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) => {
+
+ const twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] + nums[i + 1] == target) {
-            return [i, i + 1]
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
         }
     }
 }
+
